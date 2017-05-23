ed9fed4a52d97f649d98613174f7367d41761e2a
Flor Tello <flor.26071994@gmail.com>
2017-05-23 18:42:40 -0500

    Haciendo merge with header

--- js/app.js
+++ js/app.js
@@ -1,1 +1,28 @@
 $(document).foundation();
+$(document).foundation()
+$('.sim-thumb').on('mouseover', function() {
+  $('#main-product-image').attr('src', $(this).data('image'));
+})
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    dots: false,
+    navText: [
+      "<button class='orbit-previous'><span class='fa fa-angle-left'></span></button>",
+      "<button class='orbit-next'><span class='fa fa-angle-right'></span></button>"
+      ],
+    responsive:{
+        0:{
+            items:2
+
+
+        },
+        600:{
+            items:3
+        },
+        1100:{
+            items:5
+        }
+    }
+})
