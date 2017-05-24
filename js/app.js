0ab69a6a0817a6a79d8dd39ca77ba9335c65a074
Flor Tello <flor.26071994@gmail.com>
2017-05-24 13:19:23 -0500

    Fixed entre ramas

--- js/app.js
+++ js/app.js
@@ -1,4 +1,31 @@
 $(document).foundation();
+$('.sim-thumb').on('mouseover', function() {
+  $('#main-product-image').attr('src', $(this).data('image'));
+})
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    dots: false,
+    lazyLoad:false,
+    navText: [
+      "<button class='orbit-previous'><span class='fa fa-angle-left'></span></button>",
+      "<button class='orbit-next'><span class='fa fa-angle-right'></span></button>"
+      ],
+    responsive:{
+        0:{
+            items:2
+
+
+        },
+        600:{
+            items:3
+        },
+        1100:{
+            items:5
+        }
+    }
+})
 
 var principalImage = document.getElementById("principalImage"),
     image1 = document.getElementById("image1"),
